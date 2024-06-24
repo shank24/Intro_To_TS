812c214bcd4f4ae8114c7582a21559834238e6ee
Shanky Kalra <shankkalra@gmail.com>
2024-06-24 19:52:28 -0300

    Interface

--- TS_Learning/add.js
+++ TS_Learning/add.js
@@ -1,2 +1,5 @@
 var fName = "Loki";
 console.log(fName);
+//Type Annotaton
+var lName = "Poki";
+console.log(lName);
--- TS_Learning/add.ts
+++ TS_Learning/add.ts
@@ -1,2 +1,6 @@
 var fName = "Loki";
 console.log(fName);
+
+//Type Annotaton
+var lName :string = "Poki";
+console.log(lName);
